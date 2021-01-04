3a088836c397160fec381752867e6a484cbacde4
Jos van Bakel <jos@codeaddict.org>
2021-01-04 20:55:52 +0100

    WIP ani

--- src/assets/ani.rs
+++ src/assets/ani.rs
@@ -4,10 +4,9 @@ use bevy::{
     render::texture::{Extent3d, Texture, TextureDimension, TextureFormat},
     utils::BoxedFuture,
 };
-use byteorder::{ReadBytesExt, WriteBytesExt, LE};
+use byteorder::{ReadBytesExt, LE};
 use std::fmt::Debug;
-use std::io::{Write, Cursor, Read, Seek, SeekFrom};
-use std::fs::File;
+use std::io::{Cursor, Read, Seek, SeekFrom};
 
 /// Convert Bomberman specific ANI format to a sprite sheet.
 ///
@@ -52,10 +51,10 @@ impl AniItem {
     }
 }
 
-#[derive(Default)]
-struct CImg {
+struct Image {
     width: usize,
     height: usize,
+    /// 32bit RGBA encoded pixels.
     data: Vec<u8>,
 }
 
@@ -64,6 +63,63 @@ struct AniDecoder<'a> {
     cursor: Cursor<&'a [u8]>,
     has_loaded_metadata: bool,
     id: u16,
+    file_end: u64,
+}
+
+struct TgaRleIterator<'a, 'b> {
+    cursor: &'a mut Cursor<&'b [u8]>,
+    rle_data: Option<u16>,
+    rle_len: u8,
+    raw_len: u8,
+}
+
+impl<'a, 'b> TgaRleIterator<'a, 'b> {
+    fn new(cursor: &'a mut Cursor<&'b [u8]>) -> Self {
+        TgaRleIterator {
+            cursor,
+            rle_data: None,
+            rle_len: 0,
+            raw_len: 0,
+        }
+    }
+}
+
+// http://www.ludorg.net/amnesia/TGA_File_Format_Spec.html
+impl<'a, 'b> Iterator for TgaRleIterator<'a, 'b> {
+    type Item = u16;
+
+    #[inline]
+    fn next(&mut self) -> Option<u16> {
+        if self.rle_len > 0 {
+            self.rle_len -= 1;
+            return self.rle_data;
+        } else if self.raw_len > 0 {
+            self.raw_len -= 1;
+            return self.cursor.read_u16::<LE>().ok();
+        }
+
+        let control = match self.cursor.read_u8() {
+            Ok(b) => b,
+            Err(_) => return None,
+        };
+
+        // For both RLE and Raw packets we need to the next pixel value.
+        let len = control & 0b01111111;
+        let data = match self.cursor.read_u16::<LE>() {
+            Ok(b) => b,
+            Err(_) => return None,
+        };
+
+        // RLE sequence if highest bit is set.
+        if control & 0b10000000 != 0 {
+            self.rle_len = len;
+            self.rle_data = Some(data);
+            self.rle_data
+        } else {
+            self.raw_len = len;
+            Some(data)
+        }
+    }
 }
 
 impl<'a> AniDecoder<'a> {
@@ -94,23 +150,8 @@ impl<'a> AniDecoder<'a> {
         }
 
         let file_length = self.cursor.read_u32::<LE>()?;
-        let file_id = self.cursor.read_u16::<LE>()?;
-        let file_end = self.cursor.position() + file_length as u64;
-
-        println!("{} {} {}", file_length, file_id, file_end);
-        while self.cursor.position() < file_end {
-            let item = self.read_item()?;
-            println!(
-                "item {} {:?}",
-                String::from_utf8(item.signature.into())?,
-                item
-            );
-            if item.signature == *b"FRAM" {
-                self.parse_frame(item)?;
-            } else {
-                self.cursor.seek(SeekFrom::Current(item.length as i64))?;
-            }
-        }
+        let _file_id = self.cursor.read_u16::<LE>()?;
+        self.file_end = self.cursor.position() + file_length as u64;
 
         Ok(())
     }
@@ -124,9 +165,9 @@ impl<'a> AniDecoder<'a> {
         Ok(item)
     }
 
-    fn parse_frame(&mut self, frame: AniItem) -> Result<()> {
-        let mut name = None;
-        let mut cimg = None;
+    fn parse_frame(&mut self, frame: AniItem) -> Result<Image> {
+        let mut _name = None;
+        let mut image = None;
 
         while self.cursor.position() < frame.start + frame.length as u64 {
             let item = self.read_item()?;
@@ -135,48 +176,29 @@ impl<'a> AniDecoder<'a> {
                     let mut buf = vec![0; item.length as usize];
                     self.cursor.read_exact(&mut buf)?;
                     buf.pop(); // remove \0
-                    name = Some(String::from_utf8(buf)?);
+                    _name = Some(String::from_utf8(buf)?);
                 }
-                b"CIMG" => cimg = Some(self.parse_cimg(&name.as_ref().unwrap(), item)?),
+                b"CIMG" => image = Some(self.parse_image(item)?),
                 _ => item.skip(&mut self.cursor)?,
             }
         }
 
-        if name.is_some() && cimg.is_some() {
-            println!("read frame {} {}x{}", name.unwrap(), cimg.as_ref().unwrap().width, cimg.as_ref().unwrap().height);
-        }
-
-        Ok(())
+        Ok(image.unwrap())
     }
 
-    fn parse_cimg(&mut self, name: &str, item: AniItem) -> Result<CImg> {
+    fn parse_image(&mut self, item: AniItem) -> Result<Image> {
         if item.length < 32 {
             bail!("CIMG is too small: {} < 32", item.length);
         }
 
-        let mut img = CImg::default();
-
-        let bits_per_pixel = match self.cursor.read_u16::<LE>()? {
-            0x0004 => 10,
-            0x0005 => 24,
-            0x000a => 4,
-            other => bail!("Unknown CIMG type: {}", other),
-        };
-
-        if bits_per_pixel != 10 {
-            bail!("CIMG bits per pixel {} is not supported", bits_per_pixel);
+        if self.cursor.read_u16::<LE>()? != 0x0004 {
+            bail!("CIMG type must be 0x0004 (16 bits per pixel)");
         }
 
         // Unknown field.
-        self.cursor.read_u16::<LE>()?;
+        println!("{:#018b}", self.cursor.read_u16::<LE>()?);
 
         let additional_size = self.cursor.read_u32::<LE>()?;
-        if additional_size < 24 {
-            bail!("CIMG special header is too small: {} < 24", additional_size);
-        }
-        if self.cursor.position() + additional_size as u64 > item.start + item.length as u64 {
-            bail!("CIMG special header size mismatch");
-        }
         if additional_size >= 32 {
             bail!("CIMG palette header not supported");
         }
@@ -184,10 +206,10 @@ impl<'a> AniDecoder<'a> {
         // Unknown field.
         self.cursor.read_u32::<LE>()?;
 
-        img.width = self.cursor.read_u16::<LE>()? as usize;
-        img.height = self.cursor.read_u16::<LE>()? as usize;
+        let width = self.cursor.read_u16::<LE>()? as usize;
+        let height = self.cursor.read_u16::<LE>()? as usize;
         let _hotspot_x = self.cursor.read_u16::<LE>()?;
-        let _hotspot_y = self.cursor.read_u16::<LE>()?;        
+        let _hotspot_y = self.cursor.read_u16::<LE>()?;
         let _keycolor_bytes = self.cursor.read_u16::<LE>()?;
 
         // Unknown field.
@@ -200,59 +222,72 @@ impl<'a> AniDecoder<'a> {
         self.cursor.read_u16::<LE>()?;
         self.cursor.read_u16::<LE>()?;
 
-        let compressed_size = self.cursor.read_u32::<LE>()? - 12;
-        let uncompressed_size = self.cursor.read_u32::<LE>()?;
+        let _compressed_size = self.cursor.read_u32::<LE>()? - 12;
+        let _uncompressed_size = self.cursor.read_u32::<LE>()?;
 
-        // data is RLE encoded TGA data.
-        // bitsperpixel = 16 ??
+        let data = TgaRleIterator::new(&mut self.cursor)
+            .take(width * height)
+            .flat_map(|v| {
+                // For a Pixel Depth of 15 and 16 bit, each pixel is stored with 5 bits
+                // per color. If the pixel depth is 16 bits, the topmost bit is reserved
+                // for transparency.
+                if v & 0b1000_0000_0000_0000 != 0 {
+                    vec![0, 0, 0, 0]
+                } else {
+                    let r = (((v & 0b0111_1100_0000_0000) >> 10) << 3) as u8;
+                    let g = (((v & 0b0000_0011_1110_0000) >> 5) << 3) as u8;
+                    let b = ((v & 0b0000_0000_0001_1111) << 3) as u8;
+                    vec![r, g, b, 255]
+                }
+            })
+            .collect::<Vec<u8>>();
 
-        let mut data = vec![0; compressed_size as usize];
-        self.cursor.read_exact(&mut data)?;
+        // For some reason one byte is not consumed?
+        self.cursor.read_u8()?;
 
-        let mut header = vec![
-            0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0, 0
-        ];
+        println!("{}x{} {}", width, height, data.len() as usize);
 
-        let mut file = File::create(format!("out/{}", name))?;
-        file.write_all(&header)?;
-        file.write_u16::<LE>(img.width as u16)?;
-        file.write_u16::<LE>(img.height as u16)?;
-        file.write_u8(16)?; // 16 bits in a stored pixel index.
-        file.write_u8(0x20)?; // screen origin: upper left hand.
-        file.write_all(&data)?;
-
-        Ok(img)
+        Ok(Image {
+            width,
+            height,
+            data,
+        })
     }
 
     fn read_image_data(&mut self) -> Result<Texture> {
-        // let data_size = self.width as usize * self.height as usize;
-        // let pixel_indices: Vec<u8> = if self.rle {
-        //     RleIterator::new(self.cursor.clone())
-        //         .take(data_size)
-        //         .collect()
-        // } else {
-        //     let mut data = vec![0; data_size];
-        //     for value in data.iter_mut() {
-        //         *value = self.cursor.read_u8()?;
-        //     }
-        //     data
-        // };
+        let mut frames = vec![];
+        while self.cursor.position() < self.file_end {
+            let item = self.read_item()?;
+            if item.signature == *b"FRAM" {
+                frames.push(self.parse_frame(item)?);
+            } else {
+                item.skip(&mut self.cursor)?;
+            }
+        }
 
-        // self.read_palette()?;
+        let (width, height) = match frames.get(0) {
+            Some(first) => (first.width, first.height),
+            None => bail!("No frames in ANI file"),
+        };
 
-        // let palette = self.palette.as_ref().unwrap();
-        // let image_data: Vec<u8> = pixel_indices
-        //     .iter()
-        //     .flat_map(|i| {
-        //         let (r, g, b) = palette.get(*i as usize).unwrap();
-        //         vec![*r, *g, *b, 255]
-        //     })
-        //     .collect();
+        for (i, frame) in frames.iter().enumerate() {
+            println!("{} {}x{}", i, frame.width, frame.height);
+        }
 
-        let image_data = vec![0; 4];
+        if !frames
+            .iter()
+            .all(|i| i.width == width && i.height == height)
+        {
+            bail!("Frames must all have the same dimensions");
+        }
+
+        // Build sprite sheet in vertical direction. Vertical because that
+        // allows us to just concat all the image data together.
+        let num_frames = frames.len() as u32;
+        let image_data = frames.drain(0..).flat_map(|i| i.data).collect();
 
         let texture = Texture::new(
-            Extent3d::new(1, 1, 1),
+            Extent3d::new(width as u32, height as u32 * num_frames, 1),
             TextureDimension::D2,
             image_data,
             TextureFormat::Rgba8UnormSrgb,
--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,50 @@ fn main() {
         .add_plugin(CustomAssetsPlugin)
         .add_startup_system(setup.system())
         .add_startup_system(load_and_play_audio.system())
+        .add_system(animate_sprite_system.system())
         .run();
+}
+
+fn animate_sprite_system(
+    time: Res<Time>,
+    texture_atlases: Res<Assets<TextureAtlas>>,
+    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
+) {
+    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
+        timer.tick(time.delta_seconds());
+        if timer.finished() {
+            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
+            sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
+        }
+    }
 }
 
 fn setup(
     commands: &mut Commands,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    // let texture_handle = asset_server.load("data/RES/MAINMENU.PCX");
-    let texture_handle = asset_server.load("data/ANI/HEADWIPE.ANI");
+    // let background_handle = asset_server.load("data/RES/MAINMENU.PCX");
+
     // let texture_handle = asset_server.load("data/ANI/BWALK3.ANI");
+    // let texture_handle = asset_server.load("data/ANI/KFACE.ANI");
+    let texture_handle = asset_server.load("data/ANI/HEADWIPE.ANI");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(73.0, 73.0), 1, 210);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
     commands
         .spawn(Camera2dBundle::default())
-        .spawn(SpriteBundle {
-            material: materials.add(texture_handle.into()),
+        // .spawn(SpriteBundle {
+        //     material: materials.add(background_handle.into()),
+        //     ..Default::default()
+        // })
+        .spawn(SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            transform: Transform::from_scale(Vec3::splat(6.0)),
             ..Default::default()
-        });
+        })
+        .with(Timer::from_seconds(1. / 25., true));
 }
 
 fn load_and_play_audio(asset_server: Res<AssetServer>, audio: Res<Audio>) {
